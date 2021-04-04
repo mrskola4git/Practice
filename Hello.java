dc73435ab54c1911a2f8bbc63748a0ea7ac44317
sureshkola <mrskola4help@gmail.com>
2021-04-04 05:25:35 +0000

    via gui change

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ class Hello{
 public static void main(String args[]){  
 
      System.out.println("Hello Java child");  
+     System.out.println("Hello Java child2");  
+     System.out.println("gui");  
 
     }
 
