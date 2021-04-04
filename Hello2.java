80a78674da84afa67406cd797caccdba4ca92cbe
sureshkola <mrskola4help@gmail.com>
2021-04-04 05:35:39 +0000

    Hello2.java added

---
+++ Hello2.java
@@ -0,0 +1,10 @@
+class Hello2{
+public static void main(String args[]){  
+
+     System.out.println("Hello Java child");  
+     System.out.println("Hello Java child2");  
+     System.out.println("gui2");  
+
+    }
+
+}
